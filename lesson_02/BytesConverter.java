9ea921896f28387e9578933f8dff710a13bc4215
toster <35506252+tosterBadan@users.noreply.github.com>
2018-04-04 21:01:12 +0300

    update binares operations

--- lesson_02/BytesConverter.java
+++ lesson_02/BytesConverter.java
@@ -3,10 +3,10 @@ public class BytesConverter {
 
         long size = Long.parseLong(args[0]);
 
-        long gb = (size & 0xFF000000) >> 24;
-        long mb = (size & 0x00FF0000) >> 16;
-        long kb = (size & 0x0000FF00) >> 8;
-        long by = (size & 0x000000FF);
+        long gb = (size & (0x3FF<<30)) >> 30;
+        long mb = (size & (0x3FF<<20)) >> 20;
+        long kb = (size & (0x3FF<<10)) >> 10;
+        long by = (size & 0x3FF);
 
         System.out.println(args[0] + "bytes is: " + gb + "Gb " + mb + "Mb " + kb + "kb  " + by + "bytes");
     }
