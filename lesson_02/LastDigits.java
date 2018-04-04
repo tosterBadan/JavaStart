00831953f48c4d473619d8fb7f8173f9d574a263
tosterBadan <toster.testerovich@gmail.com>
2018-04-04 17:23:31 +0300

    update LastDigits

--- lesson_02/LastDigits.java
+++ lesson_02/LastDigits.java
@@ -1,5 +1,5 @@
 public class LastDigits {
     public static void main(String[] args) {
-        System.out.println("last 5 symbols: " + args[0].subSequence(args[0].length() - 5, args[0].length()));
+        System.out.println("last 5 symbols: " + args[0].substring(args[0].length() - 5));
     }
 }
