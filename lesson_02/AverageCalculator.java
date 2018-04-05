3981a56f88659f779a6131447119e5dc8f28372e
toster <35506252+tosterBadan@users.noreply.github.com>
2018-04-05 17:00:27 +0300

    change long to int

--- lesson_02/AverageCalculator.java
+++ lesson_02/AverageCalculator.java
@@ -1,8 +1,10 @@
 public class AverageCalculator {
     public static void main(String[] args) {
-        long max = Math.max(Long.parseLong(args[0]),Long.parseLong(args[1]));
-        long min = Math.min(Long.parseLong(args[0]),Long.parseLong(args[1]));
+        
+        int max = Math.max(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+        int min = Math.min(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
 
         System.out.println(min+(max-min)/2);
+    }
     }
 }
